f9b8dee495852f2ce8b64a560f6433595302c98f
Sergio Aceituno <saceituno92@gmail.com>
2026-02-09 19:16:33 +0100

    feat(database): add property_images model, relations and migrations

--- backend/app/infrastructure/database/models/__init__.py
+++ backend/app/infrastructure/database/models/__init__.py
@@ -1,3 +1,4 @@
 from app.infrastructure.database.models.user import User
 from app.infrastructure.database.models.client import Client
 from app.infrastructure.database.models.property import Property
+from app.infrastructure.database.models.property_image import PropertyImage
