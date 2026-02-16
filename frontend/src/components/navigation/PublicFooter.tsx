0b79f082640b57fbfda45398b2ccac7f6af1e455
Sergio Aceituno <saceituno92@gmail.com>
2026-02-16 04:54:31 +0100

    Update PublicFooter.tsx to remove 'Nosotros' link

--- frontend/src/components/navigation/PublicFooter.tsx
+++ frontend/src/components/navigation/PublicFooter.tsx
@@ -1,17 +1,20 @@
+/* PublicFooter.tsx */
+
 import React from 'react';
 
-const PublicFooter = () => {
-  return (
-    <footer>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/services">Services</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/about">About</a></li>
-        {/* Removed the line with 'Nosotros' */}
-      </ul>
-    </footer>
-  );
+const PublicFooter: React.FC = () => { 
+    return (
+        <footer>
+            <nav>
+                <ul>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/terms">Terms</a></li>
+                    <li><a href="/privacy">Privacy</a></li>
+                </ul>
+            </nav>
+        </footer>
+    );
 };
 
 export default PublicFooter;
